e5627c4171da5cba6adc13e2c6dd393a66b66c4a
Justi29 <61147766+Justi29@users.noreply.github.com>
2024-05-29 19:54:43 +0200

    add Application, Domain and Infrastructure

---
+++ FirmaKurierska.Domain/Exceptions/BadRequestException.cs
@@ -0,0 +1,8 @@
+﻿namespace FirmaKurierska.Domain.Exceptions
+{
+    // Wyjątek: żądanie użytkownika zawiera błędy
+    public class BadRequestException : Exception
+    {
+        public BadRequestException(string message) : base(message) { }
+    }
+}
