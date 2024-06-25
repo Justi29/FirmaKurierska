2b9c76624d8e74ef8fb916024a13ce7f19d31b3c
adapo <pterodyktal@gmail.com>
2024-06-25 19:55:35 +0200

    edit courier details

--- FirmaKurierska.BlazorClient/Services/CourierService.cs
+++ FirmaKurierska.BlazorClient/Services/CourierService.cs
@@ -1,11 +1,17 @@
-﻿using FirmaKurierska.SharedKernel.Dto;
+﻿using FirmaKurierska.Domain.Exceptions;
+using FirmaKurierska.Domain.Models;
+using FirmaKurierska.SharedKernel.Dto;
+using Microsoft.AspNetCore.Components;
 using Newtonsoft.Json;
+using System.Net.Http;
+using System.Text;
 
 namespace FirmaKurierska.BlazorClient.Services
 {
     public interface ICourierService
     {
         Task<CourierDto> GetById(int id);
+        Task Update(CourierDto dto);
     }
     public class CourierService : ICourierService
     {
@@ -27,5 +33,17 @@ namespace FirmaKurierska.BlazorClient.Services
             }
             return new CourierDto();
         }
+
+        public async Task Update(CourierDto dto)
+        {
+            var json = JsonConvert.SerializeObject(dto);
+            var data = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var response = await _httpClient.PutAsync($"http://localhost:5218/Courier/{dto.Id}", data);
+            response.EnsureSuccessStatusCode();
+        }
+
+
+
     }
 }
