37617c2502b30aa7d9accaab182bf5351b21d642
OlaWitko <79855260+OlaWitko@users.noreply.github.com>
2024-06-25 20:34:39 +0200

    edit and delete address for client methods

--- FirmaKurierska.BlazorClient/Services/AddressService.cs
+++ FirmaKurierska.BlazorClient/Services/AddressService.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
+using FirmaKurierska.Domain.Exceptions;
 using FirmaKurierska.SharedKernel.Dto;
 using Newtonsoft.Json;
 
@@ -9,6 +11,10 @@ namespace FirmaKurierska.BlazorClient.Services
     public interface IAddressService
     {
         Task<IEnumerable<AddressDto>> GetAllForClient(int clientId);
+        Task<AddressDto> GetById(int id);
+        Task UpdateAddress(UpdateAddressDto addressDto);
+        Task DeleteAddress(int id);
+        //Task CreateAddress(CreateAddressDto addressDto);
     }
     public class AddressService : IAddressService
     {
@@ -30,6 +36,54 @@ namespace FirmaKurierska.BlazorClient.Services
             }
             return new List<AddressDto>();
         }
+
+        public async Task<AddressDto> GetById(int id)
+        {
+            var response = await _httpClient.GetAsync($"http://localhost:5218/Address/{id}");
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                var address = JsonConvert.DeserializeObject<AddressDto>(content);
+                return address;
+            }
+            return null;
+        }
+
+        public async Task UpdateAddress(UpdateAddressDto addressDto)
+        {
+            if (addressDto == null || addressDto.Id <= 0)
+            {
+                throw new BadRequestException("Invalid address data");
+            }
+
+            var content = new StringContent(JsonConvert.SerializeObject(addressDto), Encoding.UTF8, "application/json");
+            var response = await _httpClient.PutAsync($"http://localhost:5218/Address/{addressDto.Id}", content);
+            response.EnsureSuccessStatusCode(); // Throw an exception if not successful
+        }
+
+        public async Task DeleteAddress(int id)
+        {
+            if (id <= 0)
+            {
+                throw new BadRequestException("Invalid address ID");
+            }
+
+            var response = await _httpClient.DeleteAsync($"http://localhost:5218/Address/{id}");
+            response.EnsureSuccessStatusCode(); // Throw an exception if not successful
+        }
+
+        //public async Task CreateAddress(CreateAddressDto addressDto)
+        //{
+        //    if (addressDto == null)
+        //    {
+        //        throw new BadRequestException("Invalid address data");
+        //    }
+
+        //    var content = new StringContent(JsonConvert.SerializeObject(addressDto), Encoding.UTF8, "application/json");
+        //    var response = await _httpClient.PostAsync($"http://localhost:5218/Address", content);
+        //    response.EnsureSuccessStatusCode(); // Throw an exception if not successful
+        //}
     }
+
 
 }
